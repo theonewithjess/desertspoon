643ed39bbc5658317a10c775707b69172838f6e9
Austin Overton <austinsoverton@gmail.com>
2019-01-14 10:29:58 -0700

    chartstuff

--- src/App.js
+++ src/App.js
@@ -3,14 +3,25 @@ import './App.css';
 import {connect} from 'react-redux'
 import {Switch, Route, withRouter} from 'react-router-dom'
 import DailyLog from './components/DailyLog'
+import LineChart from './components/LineChart'
+import Chart from './components/Chart'
 
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      chartData:{},
+      lineChartData:{},
+    }
+  }
   render() {
     return (
       <div className="App">
         <h1>desertspoon.com</h1>
         <DailyLog />
+        {/*<Chart chartData={this.state.chartData} legendPosition="right"/>*/}
+        {/*<LineChart lineChartData={this.state.LineChartData} legendPosition="right"/>*/}
       </div>
     );
   }
